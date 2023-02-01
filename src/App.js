2547a862d247377c360bb73ae41040c39b243eab
natussshka <112900249+natussshka@users.noreply.github.com>
2023-02-01 20:02:27 +0100

    created html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <Weather defaultCity="Vienna" />
+
         <footer>
           This project was coded by Natalia Kubasi and is{" "}
           <a href="https://github.com/natussshka/react-weather" target="_blank">
